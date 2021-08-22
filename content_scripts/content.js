28f977af3a09f712d6cc14233755802952242cf7
Tony Kostowny <tonykostowny@gmail.com>
2021-08-22 12:08:30 -0700

    add open top link on ddg in a new tab

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -4,12 +4,11 @@ browser.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.text === 'report_back') {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
-
-        // This will log in the tab console.  This is good 
-        var h5linkHref = document.body.querySelector('h5').querySelector('a').href;
-        console.log(h5linkHref);
         // sendResponse(document.all[0].outerHTML);
-        sendResponse(h5linkHref);
-        
+
+      var linkElement = document.getElementById("links");
+      var link = linkElement.querySelector('a');
+      var hrefAsString = link.getAttribute('href');
+      sendResponse(hrefAsString);
     }
 });
