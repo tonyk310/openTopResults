aa813476e84152543fcb50325e9600db229ec373
Tony Kostowny <tonykostowny@gmail.com>
2021-08-26 10:51:23 -0700

    remove 4 "three icons" add 3 "ribbon" icons, change manifest to reflect change

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?duckduckgo\.com/;
 function createTabLinks(domContent) {
 
   var parsedLinkStringArray = JSON.parse(domContent);
-  console.log(parsedLinkStringArray);
+  // console.log(parsedLinkStringArray);
 
   for (var i = 0; i < parsedLinkStringArray.length; i++) {
     var currentLinkString = parsedLinkStringArray[i];
@@ -20,11 +20,59 @@ function createTabLinks(domContent) {
   }
 }
 
+
 // When the browser-action button is clicked...
+
 browser.browserAction.onClicked.addListener(function (tab) {
+  
     // ...check the URL of the active tab against our pattern and...
     if (urlRegex.test(tab.url)) {
         // ...if it matches, send a message specifying a callback too
         browser.tabs.sendMessage(tab.id, {text: 'message_received'}, createTabLinks);
     }
 });
+
+
+
+
+// From Beastify
+
+// browser.tabs.executeScript({file: "/content_scripts/content.js"})
+// .then(function() {
+//   // When the browser-action button is clicked...
+//   browser.browserAction.onClicked.addListener(function (tab) {
+//     browser.tabs.sendMessage(tab.id, {text: 'message_received'}, createTabLinks);
+//   });  
+// });
+
+// browser.tabs.executeScript({file: "/content_scripts/content.js"})
+// .then(function() {
+//   console.log("Hello from background.js, the promise was returned."); 
+// }).catch(function() {
+//   console.log("We CAUGHT an error.");
+// });
+
+// Stack Overflow
+// var updateTextTo = document.getElementById('comments').value;
+// browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   console.log(tabs);
+//     browser.tabs.executeScript(tabs[0].id, {
+//         file: "/content_scripts/content.js"
+//     }, function(){
+//         browser.tabs.sendMessage(
+//           tabs[0].id, { text: 'message_received' }, createTabLinks
+//         );
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,35 +1,7 @@
 // Listen for messages
-browser.runtime.onMessage.addListener(function (msg, sender, callback) {
+browser.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
     if (msg.text === 'message_received') {
-      // Call the specified callback, passing
-      // the web-page's DOM content as argument
-      // sendResponse(document.all[0].outerHTML);
-
-      // Grab one link
-      // var linkElement = document.getElementById("links");
-      // var link = linkElement.querySelector('a');
-      // var hrefAsString = link.getAttribute('href');
-      // sendResponse(hrefAsString);
-
-      // console.log("message_received");
-
-      // var linkStringArray = [];
-      // // Get the set of links on the page
-      // var h2Set = document.getElementsByTagName('h2');
-      // // for each element 'currentLink' get the href
-      // for (var i = 0; i < 3; i++) {
-      //   var currentH2Object = h2Set[i];
-      //   var currentLinkString = currentH2Object.querySelector('a').href;
-        
-      //   console.log(currentH2Object);
-
-      //   linkStringArray.push(currentLinkString);
-      // }
-
-      // var serializedLinkStringArray = JSON.stringify(linkStringArray);
-      // callback(serializedLinkStringArray);
-
 
       var linkStringArray = [];
       // Get the set of links contained within the h2's on the page.
@@ -42,8 +14,9 @@ browser.runtime.onMessage.addListener(function (msg, sender, callback) {
         linkStringArray.push(currentLinkString);
       }
 
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage#addlistener_syntax
       var serializedLinkStringArray = JSON.stringify(linkStringArray);
-      callback(serializedLinkStringArray);
+      sendResponse(serializedLinkStringArray);
 
       //////////////////////////////
       // GOOGLE //
